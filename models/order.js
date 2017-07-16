edb634529c6bfa9683c8206a46e064255a963173
ManaPoustizadeh <mana0pst@gmail.com>
2017-07-16 14:03:46 +0430

    fix(DB): changes models
    changes order model and food model in order to make multilevel population possible

--- models/order.js
+++ models/order.js
@@ -6,7 +6,7 @@ class Order extends Model{
     static get $schema() {
         return {
             user: {type: Schema.Types.ObjectId, ref: 'User'},
-            foods: [{type: Schema.Types.ObjectId, ref: 'Food'}``],
+            foods: [{type: Schema.Types.ObjectId, ref: 'Food'}],
             price: {type: Schema.Types.Number}, //total price for an order set
             slug: {type: String}, //for minio photo
             status: {type: String, enum: ['accepted', 'in_progress', 'ready', 'sent'], default: 'accepted'} //This is for re-ordering and changing category order for the restaurant
