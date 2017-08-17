ef6d77752d627fcc970e49faf789f6e71d9ed1e5
ManaPoustizadeh <mana0pst@gmail.com>
2017-08-17 15:44:18 +0430

    fix(order): fixes bug in pagination
    moves method to admin controller

--- controllers/public/order.js
+++ controllers/public/order.js
@@ -16,22 +16,7 @@ class OrderController extends Controller{
         let orders = Order.find({}).populate('foods');
         reply(orders);
     }
-	
-    async _post(request, reply) {
-        const filter = request.payload.obj.filter;
-        const perPage = request.payload.obj.perPage;
-        const curPage = request.payload.obj.curPage;
-        let cur = curPage == 0 ? 0 : curPage-1;
-        let orders = '';
-        let filterOrder = await Order.find({status: {$in: filter}});
-        orders = await Order.find({status: {$in: filter}}).populate('foods').skip(perPage * cur).limit(perPage);
-        if(filter.length==0){
-            filterOrder = await Order.find({});
-            orders = await Order.find({}).populate('foods').skip(perPage * cur).limit(perPage);
-        }
-        reply({orders, count:filterOrder.length});
-    }
-	
+
 	 async $id_post(request, reply, {id}){
         const state = request.payload.state;
         Order.findById(id, function (err, order) {
